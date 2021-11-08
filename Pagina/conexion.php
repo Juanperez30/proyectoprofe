2e24883764705c6dac72e3485f1941b3089377bd
Natalia Jaimes <lnataliaj61@gmail.com>
2021-11-08 15:40:46 -0500

    Nuevo

--- Pagina/conexion.php
+++ Pagina/conexion.php
@@ -1,29 +1,17 @@
 <?php
 
-//$nombre_proyecto = $_POST['nombre_proyecto'];
-//$descripcion = $_POST['descripcion'];
-//$enlace = $_POST['enlace'];
-//$categoria = $_GET['categoria'];
-function conectar(){
-  $user = "root";
-  $pass = "1234";
-  $server = "localhost";
-  $db = "expotecnicos";
-  $con = mysql_connect($server, $user,$pass) or die ("error al conectar".mysql_error());
-  mysqsl_select_db($db,$con);
-  return $con;
+$mysql_host='localhost';
+$mysql_user='root';
+$mysql_password='030055';
+
+if(!@mysql_connect($mysql_host, $mysql_user, $mysql_password)){ 
+  die ('nonas');
 }
-
-
-function cargarTabla($conexion){
-  $consulta = "SELECT * FROM proyecto WHERE categoria = $categoria";
-  $resultado = mysqli_query($conexion,$consulta);
-
-  while($fila = mysqli_fetch_array($resultado)){
-    echo "<tr>";
-    echo "<td>".$fila['nombre_proyecto'];
-    echo "<td>".$fila['descripcion'];
-    echo "<td>".$fila['enlace'];
-    echo "<tr>"
+  else {
+    if(@mysql_select_db('expotecnicos')){
+      echo  'yay';
   }
+  else{
+    die ('nonas');
 }
+?>
