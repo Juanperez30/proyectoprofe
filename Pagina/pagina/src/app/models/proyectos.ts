b63d3a5fefff25dd7db00c5acb24e2bc67920314
Natalia Jaimes <lnataliaj61@gmail.com>
2021-11-08 07:57:50 -0500

    esto no

--- Pagina/pagina/src/app/models/proyectos.ts
+++
@@ -1,7 +0,0 @@
-export interface Proyecto {
-    id?: number;
-    nombre_proyecto: string;
-    descripcion: string;
-    enlace:string;
-    categoria: number
-}
--- Pagina/server/src/keys.ts
+++
@@ -1,9 +0,0 @@
-export default {
-
-    database: {
-        host: 'DESKTOP-Nat',
-        user: 'Nat',
-        password: '030055',
-        database: 'expotecnicos'
-    }
-}
